b973bc49cb950a2c1364ac0d896ade871bad57f2
Robert Perez <rickymcmuffins@gmail.com>
2024-01-24 22:28:51 -0500

    better formatting

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # COP4520HW1
+
+## Building
+
+Make sure rust is installed,
+Then run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::io;
 use std::io::Write;
 use std::sync::Arc;
@@ -6,27 +7,44 @@ use std::thread;
 use std::thread::JoinHandle;
 use std::time::Instant;
 
-const LIMIT: u64 = 10_u64.pow(8);
+const LIMIT: u64 = 10_u64.pow(7);
+const RUN_SINGLE: bool = true;
 
 fn main() {
     let now = Instant::now();
-    let (sum, num_primes) = find_primes_parallel();
-    println!("{:?}, {:?}, {}", sum, num_primes, now.elapsed().as_millis());
+    let (sum, num_primes, mut top_primes) = find_primes_parallel();
+    let elapsed = now.elapsed().as_millis();
+    top_primes.make_contiguous().sort();
+    while top_primes.len() > 10 {
+        top_primes.pop_front();
+    }
+    println!("{}, {}, {}", elapsed, num_primes, sum);
+    println!("{:?}", top_primes);
 
-    let now = Instant::now();
-    let (sum, num_primes) = find_primes_single();
-    println!("{:?}, {:?}, {}", sum, num_primes, now.elapsed().as_millis());
+    // runs same algorithm on one thread
+    if RUN_SINGLE {
+        let now = Instant::now();
+        let (sum2, num_primes2, top_primes2) = find_primes_single();
+        println!("{}, {}, {}", sum2, num_primes2, now.elapsed().as_millis());
+        println!("{:?}", top_primes2);
+    }
 }
 
-fn find_primes_single() -> (u64, u64) {
+// finds primes using a single core approach
+fn find_primes_single() -> (u64, u64, VecDeque<u64>) {
     let mut i: u64 = 2;
     let mut sum: u64 = 0;
     let mut num_primes: u64 = 0;
+    let mut top_primes: VecDeque<u64> = VecDeque::new();
 
     while i < LIMIT {
         if is_prime(i) {
             sum += i;
             num_primes += 1;
+            top_primes.push_back(i);
+            if top_primes.len() > 10 {
+                top_primes.pop_front();
+            }
         }
         if i % 1000000 == 0 {
             print_progress(i);
@@ -34,79 +52,118 @@ fn find_primes_single() -> (u64, u64) {
         i += 1;
     }
 
-    return (sum, num_primes);
+    print!("\r");
+    return (sum, num_primes, top_primes);
 }
 
-fn find_primes_parallel() -> (u64, u64) {
+// finds all prime numbers with 8 threads. returns sum, number of primes, and top 10 primes
+fn find_primes_parallel() -> (u64, u64, VecDeque<u64>) {
+    // add all threads
+    let mut handles: Vec<JoinHandle<()>> = Vec::new();
+
+    // all atomic shared memory
     let counter = Arc::new(Mutex::new(2_u64));
     let sum = Arc::new(Mutex::new(0_u64));
     let num_primes = Arc::new(Mutex::new(0_u64));
-    let mut handles: Vec<JoinHandle<()>> = Vec::new();
+    let primes_vec: VecDeque<u64> = VecDeque::new();
+    let top_primes = Arc::new(Mutex::new(primes_vec));
+
+    // add all threads to handles
     for _ in 0..8 {
         let counter = counter.clone();
         let sum = sum.clone();
         let num_primes = num_primes.clone();
-        let handle = thread::spawn(move || prime_print(counter, sum, num_primes));
+        let top_primes = top_primes.clone();
+        let handle = thread::spawn(move || prime_thread(counter, sum, num_primes, top_primes));
 
         handles.push(handle);
     }
 
+    // join all threads
     for handle in handles {
         handle.join().unwrap();
     }
-    // loop  {
-    //     let num = counter.lock().unwrap();
-    //     if *num > LIMIT{
-    //         break;
-    //     }
-    // }
+
+    // get return variables
     let sum = sum.lock().unwrap();
     let num_primes = num_primes.lock().unwrap();
+    let top_primes = top_primes.lock().unwrap().clone();
 
-    return (*sum, *num_primes);
+    print!("\r");
+    return (*sum, *num_primes, top_primes);
 }
 
-fn prime_print(counter: Arc<Mutex<u64>>, sum: Arc<Mutex<u64>>, num_primes: Arc<Mutex<u64>>) {
+// takes in all shared memory
+fn prime_thread(
+    counter: Arc<Mutex<u64>>,
+    sum: Arc<Mutex<u64>>,
+    num_primes: Arc<Mutex<u64>>,
+    top_primes: Arc<Mutex<VecDeque<u64>>>,
+) {
     let mut i: u64 = 0;
 
     while i < LIMIT {
-        {
-            let mut num = counter.lock().unwrap();
-            i = *num;
-            *num += 1;
-        }
+        // lock counter
+        let mut num = counter.lock().unwrap();
+        i = *num;
+        *num += 1;
+        std::mem::drop(num); // unlock num
+
+        // for printing progress
         if i % 1000000 == 0 {
             // println!("progress: {i}");
             print_progress(i);
         }
+
+        // don't check any numbers above LIMIT
         if i > LIMIT {
             break;
         }
 
+        // check if it is prime
         let prime = is_prime(i);
         if prime {
+            // lock and add to number of primes
             let mut pris = num_primes.lock().unwrap();
             *pris += 1;
+            std::mem::drop(pris);
+
+            // lock and add to sum
             let mut sum = sum.lock().unwrap();
             *sum += i;
+            std::mem::drop(sum);
+
+            // lock push to top primes
+            let mut tp = top_primes.lock().unwrap();
+            tp.push_back(i);
+            // if tp.len() > 10 {
+            //     tp.pop_front();
+            // }
+            std::mem::drop(tp);
         }
     }
 }
 
+// returns true if n is prime. false otherwise
 fn is_prime(n: u64) -> bool {
+    // start on 2 as 1 is not prime
     let mut i = 2;
 
+    // square root is largest number that won't already be checked
     let sqr = (n as f64).sqrt() as u64 + 1;
 
     while i < sqr {
+        // if divisble, it is not prime
         if n % i == 0 {
             return false;
         }
         i += 1;
     }
+    // not divisble by any numbers so it is prime
     return true;
 }
 
+// helper function to print the current progress
 fn print_progress(n: u64) {
     let percent = n * 100 / LIMIT;
     print!("\rProgress: {percent}%");
